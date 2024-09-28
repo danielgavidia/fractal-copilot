bfb92d226a94c2b81a49e2edd24e533485241d93
Daniel Gavidia <dgavidia1@gmail.com>
2024-09-28 16:28:53 -0400

    Added logic for Discord bot to provide PR summaries when running specific command

--- backend/discord/bot.ts
+++ backend/discord/bot.ts
@@ -1,4 +1,7 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
 
 const client = new Client({
     intents: [
@@ -14,11 +17,41 @@ client.once("ready", () => {
 });
 
 // Event when a message is sent in the server
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
     // Prevent bot from responding to itself
     if (message.author.bot) return;
-    if (message.content === "ping") {
-        message.channel.send("pong");
+
+    // Logic
+    const messageArray = message.content.split(" ");
+    if (messageArray[0] === "summary") {
+        try {
+            const res = await axios({
+                method: "GET",
+                url: `${BASE_URL}/github/pr-ids/danielgavidia/${messageArray[1]}/closed`,
+            });
+
+            const data = res.data;
+            console.log(data);
+
+            interface Block {
+                number: number;
+                link: string;
+                summary: string;
+            }
+
+            const getMessage = (block: Block) => {
+                const msg = `- **[PR ${block.number.toString().padStart(3, "0")}](<${
+                    block.link
+                }>)**: ${block.summary}`;
+                return msg;
+            };
+            const messageFormatted = data.map((x: Block) => getMessage(x)).join("\n");
+            console.log(messageFormatted);
+            message.channel.send(messageFormatted);
+        } catch (error) {
+            console.log(error);
+            message.channel.send("Error");
+        }
     }
 });
 
